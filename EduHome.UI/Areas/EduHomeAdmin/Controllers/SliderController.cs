2899076fead090d2f8574a722fc4652aee4157f2
Leman <leman@Leman-MacBook-Air.local>
2023-06-28 12:14:08 +0400

    admin panel is creating

---
+++ EduHome.UI/Areas/EduHomeAdmin/Controllers/SliderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHome.UI.Areas.EduHomeAdmin.Controllers;
+[Area("EduHomeAdmin")]
+
+public class SliderController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public SliderController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Sliders.ToListAsync());
+    }
+}
+
